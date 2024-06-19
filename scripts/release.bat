4d4b44139475013603a101cd824b1a8920cace8e
Devil4ngle <101042789+Devil4ngle@users.noreply.github.com>
2024-06-19 11:36:38 +0200

    Update release.bat

--- scripts/release.bat
+++ scripts/release.bat
@@ -8,6 +8,32 @@ rmdir /s /q dist
 call npm run build:prod
 cd ..
 
+if exist gh-pages rmdir /s /q gh-pages
+
+mkdir gh-pages
+
+git clone --branch gh-pages --single-branch https://github.com/Devil4ngle/squadmortar gh-pages --depth=1
+
+cd gh-pages
+
+for /F "delims=" %%i in ('dir /b') do (if not "%%i"=="\.git" rmdir /s /q "%%i" 2>nul & del /f /q "%%i" 2>nul)
+
+cd .. 
+
+xcopy /s /e frontend\dist gh-pages\
+
+cd gh-pages
+
+git add .
+
+git commit -m "Update gh-pages"
+
+git push origin gh-pages
+
+echo WebPage updated successfully.
+
+cd .. 
+
 python -m PyInstaller --onefile --noconsole --icon=config/icon.ico --name squadMortarOverlay squad_mortar_overlay.py 
 
 if exist release rmdir /s /q release
@@ -15,7 +41,7 @@ if exist release rmdir /s /q release
 :: Create the 'release' folder
 mkdir release
 
-git clone --no-checkout https://github.com/Devil4ngle/squadmortar-release.git release
+git clone --depth=1 --no-checkout https://github.com/Devil4ngle/squadmortar-release.git release
 
 :: Copy squadmortar.exe to the 'release' folder
 copy VERSION.txt release
@@ -23,15 +49,8 @@ copy VERSION.txt release
 mkdir release\config
 
 :: Copy only the specified files to the 'release' folder
-copy /y dist\squadMortarOverlay.exe release\
 copy /y config\text.txt release\config
 copy /y config\icon.ico release\config
-
-:: Create the 'frontend' folder inside 'release'
-mkdir release\frontend\dist
-
-:: Copy 'frontend/dist' to 'release\frontend'
-xcopy /s /e frontend\dist release\frontend\dist
 
 :: Initialize a new Git repository in the 'release' folder
 cd release
