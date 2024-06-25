70ae2b3cf4224394e06a6e6ce3770d531a1fd80d
--global <isaakpocchiesa@gmail.com>
2024-06-25 10:15:09 +0200

    Comment out PyInstaller command in release script

--- scripts/release.bat
+++ scripts/release.bat
@@ -34,7 +34,7 @@ echo WebPage updated successfully.
 
 cd .. 
 
-python -m PyInstaller --add-data=frontend/src/img/favicons/favicon.ico;. --clean --onefile --noconsole --icon=frontend/src/img/favicons/favicon.ico --name squadMortarOverlay squad_mortar_overlay.py 
+REM python -m PyInstaller --add-data=frontend/src/img/favicons/favicon.ico;. --clean --onefile --noconsole --icon=frontend/src/img/favicons/favicon.ico --name squadMortarOverlay squad_mortar_overlay.py 
 
 echo Task completed successfully.
 
